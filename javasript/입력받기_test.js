25da9a974aff3e4b4959bd0fa385f1e53e8d23c8
yacoblee <68984086+yacoblee@users.noreply.github.com>
2024-05-14 00:25:01 +0900

    자바스크립트 학습

--- javasript/입력받기_test.js
+++ javasript/입력받기_test.js
@@ -10,16 +10,14 @@ let sum = 0;
 //     if (one >= two) {
 //         sum = one - two
 //         alert(`${one} 과 ${two}의 차이는 ${sum} 입니다.`);
-//         if (confirm('창 닫을까요')) {
-//             break;
-//         }
+//         if (confirm('창 닫을까요')) break;
 //     }
+
 
 //     if (two >= one) {
 //         sum = two - one
 //         alert(`${one} 과 ${two}의 차이는 ${sum} 입니다.`);
-//         alert('창 닫을까요?');
-//         break;
+//         if (confirm('창 닫을까요')) break;
 //     }
 // }
 
@@ -34,7 +32,6 @@ let sum = 0;
 //     if (one > two) {
 //         alert(`큰수는 ${one} 입니다.`);
 //         if (confirm('창 닫을까요')) break;
-
 //     }
 
 //     if (two > one) {
@@ -48,24 +45,26 @@ let sum = 0;
 // }
 
 
-for (let name, one, two, three, ave; ;) {
-    name = +prompt('정수1 :');
-    one = +prompt('국어점수 :');
-    two = +prompt('영어점수 :');
-    three = +prompt('수학점수 :');
+// for (let name, one, two, three, ave; ;) {
+//     name = prompt('이름 :');
+//     one = +prompt('국어점수 :');
+//     two = +prompt('영어점수 :');
+//     three = +prompt('수학점수 :');
 
-    sum = (one + two + three) / 3;
-
-
-    if (sum >= 90) {
-        ave = A
-    }else if (90 > sum >= 80) {
-        ave = B
-    }else {
-        ave = C
-    };
-
-    alert(`학생명:${name}, 학점: ${ave}  평균은: ${sum} 입니다.`);
+//     sum = Math.floor((one + two + three) / 3);
 
 
-}
+//     if (sum >= 90) {
+//         ave = 'A'
+//     } else if (90 > sum >= 80) {
+//         ave = 'B'
+//     } else if (80 > sum >= 70) {
+//         ave = 'C'
+//     } else {
+//         ave = 'F'
+//     };
+
+//     alert(`학생명:${name}, 학점: ${ave}  평균은: ${sum} 입니다.`);
+
+
+// }
--- javasript/입력받기_test2.js
+++ javasript/입력받기_test2.js
@@ -80,12 +80,47 @@ step3 : '$' + 처음 3자리 + 총 글자 수 + url 의 14번째 문자.        
 ※ url 의 14번째 문자가 존재하지 않을 때는 ? 문자로 표시.            =>      $dau4?
 */
 
-let url = 'http://daum.net';
+// let url = 'http://daum.net';
 
-let step1, step2, step3;
-step1 = url.split('//')[1];
-console.log(step1);
+// let step1, step2, step3;
+// step1 = url.split('//')[1];
+// step2 = step1.split('.')[0];
 
-step2 = step1.split('.')[0];
-console.log(step2);
+// if (url.length >= 13) {
+//     step3 = url.slice(13, 14);
+//     console.log('$' + step2.slice(0, 3) + step2.length + step3);
+// } else {
+//     console.log('$' + step2.slice(0, 3) + step2.length + '?');
 
+// }
+
+
+/* 
+    < 문제 >
+
+- 입력받은 값이 숫자인지 아닌지를 평가하는 프로그램 구현.
+  숫자 입력 시 입력없이 "확인" 버튼을 누르거나 "취소" 버튼을 누르는
+  상황에 대한 예외 처리는 제외.
+*/
+
+let num = prompt("숫자 입력: ");
+num = num.trim();
+
+if (confirm('입력된 숫자가 맞습니까?')) {
+    let val = num.split('');
+
+    for (let i = 0, tes; i < val.length; i++) {
+        tes = Number(val[i]);
+
+        if (isNaN(tes)) {
+            alert('숫자가 아닙니다. 프로그램을 종료합니다.');
+            break;
+        }
+        if (i === val.length - 1) {
+            alert('입력한 값이 숫자가 맞습니다.');
+        }
+    }
+
+} else {
+    alert('사용자가 취소했습니다. 프로그램을 종료합니다.');
+}

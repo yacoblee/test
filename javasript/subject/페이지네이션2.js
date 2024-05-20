7366a2bfd63e3459bdb915d9b1d6fce322e4d4a9
yacoblee <68984086+yacoblee@users.noreply.github.com>
2024-05-21 00:06:43 +0900

    zjalt

--- javasript/subject/페이지네이션2.js
+++ javasript/subject/페이지네이션2.js
@@ -1,62 +1,70 @@
 const container = document.querySelector('.nation_container');
-const data = ['../../image/bg1.jpg',
-    '../../image/bg2.jpg',
-    '../../image/bg3.jpg',
-    '../../image/bg4.jpg',
-    '../../image/bg5.jpg'];
+const data = [
+    '../image/bg1.jpg',
+    '../image/bg2.jpg',
+    '../image/bg3.jpg',
+    '../image/bg4.jpg',
+    '../image/bg5.jpg'
+];
+let currentIndex = 0;
+let intervalId;
+let isRunning = true;
 
-for (let i = 1; i <= data.length; i++) {
+// Create links
+for (let i = 0; i < data.length; i++) {
     const link = document.createElement('a');
-    link.href = `#`;
-    link.textContent = `${i}`;
-    link.setAttribute('class', `nationBox nation${i}`);
+    link.href = '#';
+    link.textContent = `${i + 1}`;
+    link.setAttribute('class', `nationBox`);
+    link.dataset.index = i; // Store the index in a data attribute
     container.appendChild(link);
 }
 
+const img = document.querySelector('#main_container img');
+const links = document.querySelectorAll('.nationBox');
 
-document.addEventListener("DOMContentLoaded", function () {
-    let nav = document.querySelectorAll('a');
-    let img = document.querySelector('img');
-    const links = document.querySelectorAll('.nationBox');
+// Function to change image
+const changeImage = () => {
+    currentIndex = (currentIndex + 1) % data.length;
+    img.src = data[currentIndex];
+    links.forEach(link => link.classList.remove('nation1'));
+    links[currentIndex].classList.add('nation1');
+};
 
-    links.forEach(link => {
-        link.addEventListener('click', (e) => {
-            e.preventDefault();
-            links.forEach(link => link.classList.remove('nation1'));
-            link.classList.add('nation1');
-        });
-    }); 8
-    nav[0].addEventListener("click", function () {
-        img.src = data[0];
-    });
-    nav[1].addEventListener("click", function () {
-        img.src = data[1];
-    });
-    nav[2].addEventListener("click", function () {
-        img.src = data[2];
-    });
-    nav[3].addEventListener("click", function () {
-        img.src = data[3];
-    });
-    nav[4].addEventListener("click", function () {
-        img.src = data[4];
-    });
+// Start automatic image change
+const startInterval = () => {
+    intervalId = setInterval(changeImage, 2000);
+};
 
+// Stop automatic image change
+const stopInterval = () => {
+    clearInterval(intervalId);
+};
+
+// Toggle auto change on image click
+img.addEventListener('click', () => {
+    if (isRunning) {
+        stopInterval();
+    } else {
+        startInterval();
+    }
+    isRunning = !isRunning;
 });
 
+// Event listeners for links
+links.forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        stopInterval();
+        currentIndex = parseInt(this.dataset.index);
+        img.src = data[currentIndex];
+        links.forEach(link => link.classList.remove('nation1'));
+        this.classList.add('nation1');
+        if (isRunning) {
+            startInterval();
+        }
+    });
+});
 
-
-// document.addEventListener("click", () => {
-//     clearInterval();
-// });
-
-// document.addEventListener("click", () => {
-//     loopInterval = setInterval(() => {
-//         nextMove(); // 다음 슬라이드를 보여주는 함수
-//     }, 3000);
-// });
-
-
-
-
-
+// Initialize
+startInterval();

3cc6f6006c1da81e3895ad59d6c0b9adaed7b8b9
yacoblee <68984086+yacoblee@users.noreply.github.com>
2024-06-10 01:46:05 +0900

    개발 테스트중

--- projectTest/main.js
+++ projectTest/main.js
@@ -1,58 +1,12 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//     const header = document.querySelector("header");
-
-//     if (!header) {
-//         console.error("Header element not found");
-//         return;
-//     }
-
-//     const observer = new IntersectionObserver((entries) => {
-//         entries.forEach(entry => {
-//             if (entry.intersectionRatio > 0) {
-//                 header.classList.remove('sticky');
-//             } else if (!header.classList.contains(".sticky")) {
-//                 header.classList.add('sticky');
-//             }
-//         });
-//     }, { threshold: 0 });
-
-//     observer.observe(header);
-
-//     console.log("IntersectionObserver started");
-// });
-$(document).ready(function () {
+document.addEventListener('DOMContentLoaded', function () {
     window.$app = {};
-    // 스크롤 시 header fade-in
-    $(document).on('scroll', function () {
-        if ($(window).scrollTop() > 10) {
-            $("#header").removeClass("deactive");
-            $("#header").addClass("active");
+    document.addEventListener('scroll', function () {
+        if (window.scrollY > 10) {
+            document.getElementById("header").classList.remove("deactive");
+            document.getElementById("header").classList.add("active");
         } else {
-            $("#header").removeClass("active");
-            $("#header").addClass("deactive");
+            document.getElementById("header").classList.remove("active");
+            document.getElementById("header").classList.add("deactive");
         }
-    })
-
+    });
 });
-
-// document.addEventListener("DOMContentLoaded", () => {
-//     const sections = document.querySelectorAll(".section");
-
-//     const observerOptions = {
-//         root: null,
-//         threshold: 0.1,
-//     };
-
-//     const observer = new IntersectionObserver((entries, observer) => {
-//         entries.forEach(entry => {
-//             if (entry.isIntersecting) {
-//                 entry.target.classList.add("in-view");
-//                 observer.unobserve(entry.target);
-//             }
-//         });
-//     }, observerOptions);
-
-//     sections.forEach(section => {
-//         observer.observe(section);
-//     });
-// });
